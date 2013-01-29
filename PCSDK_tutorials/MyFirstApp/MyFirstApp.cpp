19c3f2e2b43502f4b310701ed6ec44eda0152db4
TAMURA Takashi <cslroot@gmail.com>
2013-01-30 02:32:38 +0900

    mod: color and depth
    Tutorial "My First App" is Finished.

--- PCSDK_tutorials/MyFirstApp/MyFirstApp.cpp
+++ PCSDK_tutorials/MyFirstApp/MyFirstApp.cpp
@@ -9,15 +9,21 @@
 int _tmain(int argc, _TCHAR* argv[])
 {
 	UtilPipeline pipeline;
-	pipeline.EnableImage(PXCImage::COLOR_FORMAT_RGB32);
+	pipeline.EnableImage(PXCImage::COLOR_FORMAT_RGB32, 1280, 720);	// default 640x480
+	pipeline.EnableImage(PXCImage::COLOR_FORMAT_DEPTH);
 	pipeline.Init();
 
 	UtilRender color_render(L"Color Stream");
+	UtilRender depth_render(L"Depth Stream");
+
 	for (;;) {
 		if (!pipeline.AcquireFrame(true)) break;
 
 		PXCImage *color_image = pipeline.QueryImage(PXCImage::IMAGE_TYPE_COLOR);
+		PXCImage *depth_image = pipeline.QueryImage(PXCImage::IMAGE_TYPE_DEPTH);
 		if (!color_render.RenderFrame(color_image)) break;
+		if (!depth_render.RenderFrame(depth_image)) break;
+		
 		pipeline.ReleaseFrame();
 	}
 	pipeline.Close();
